a6d26ae9a415a643f2c5531c3f7a9560c835b1bf
C-Hipple <Chris.Hipple@gmail.com>
2017-07-19 23:04:53 -0400

    pulled some oil data and started processing GDELT dumps"

--- FeatureEngineering/GDELT/get_GDELT.py
+++ FeatureEngineering/GDELT/get_GDELT.py
@@ -8,11 +8,30 @@ import os
 
 url = "http://data.gdeltproject.org/gdeltv2/20170718201500.export.CSV.zip"
 
-def get_file(url):
-    """Downloads the GDELT 2.0 file at the given URL"""
-    subprocess.call(["wget", url])
+def get_file(file_date, version, time):
+    """
+    Downloads the GDELT file at the given URL
 
+    Expects Date as ISO 8601 Format: YYYY-MM-DD
+    """
 
+    if version == 1:
+        print(file_date)
+
+        file_date = file_date.isoformat()
+        file_date = str(file_date).replace("-", "")
+        url = "http://data.gdeltproject.org/events/" + file_date + ".export.CSV.zip"
+        dl_name = "data/" + file_date + ".CSV.zip"
+        print(url)
+    else:
+        raise NotImplementedError("yeah yeah we'll get there")
+    if dl_name not in os.listdir(os.getcwd()):
+        subprocess.call(["wget", "-O", dl_name, url])
+    else:
+        print("Already downloaded file")
+
+# 1.0 example http://data.gdeltproject.org/events/20170718.export.CSV.zip
+# 2.0 example http://data.gdeltproject.org/gdeltv2/20170718201500.export.CSV.zip
 if __name__ == "__main__":
     if "20170718201500.export.CSV.zip" not in os.listdir(os.getcwd()):
         get_file(url)
