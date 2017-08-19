d7034e1ee263e06cecf1ddea136d8a10a0e6c265
root <root@storm1.bigforecast.mids>
2017-08-19 12:36:03 -0500

    working storm example topo

--- storm/topologies/gdelt_ingestion.py
+++ storm/topologies/gdelt_ingestion.py
@@ -17,13 +17,13 @@ class gdeltTopology(Topology):
     article_spout = SampleArticleSpout.spec(par=1, name="gdelt-spout")
 
     scraper_bolt = ScraperBolt.spec(inputs=[article_spout],
-                                    par=8,
+                                    par=1,
                                     name='web-scraper-bolt')
 
     analyzer_bolt = AnalyzerBolt.spec(inputs=[scraper_bolt],
-                                    par=8,
+                                    par=1,
                                     name='analyzer-bolt')
 
     loader_bolt = ESLoaderBolt.spec(inputs=[analyzer_bolt],
-                                    par=8,
+                                    par=1,
                                     name='loader-bolt')
