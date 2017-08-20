1cd47f9d8b5efb5e07ceed76b426f1fbe944896f
James Lamb <jaylamb20@gmail.com>
2017-08-20 11:38:02 -0500

    Updated mapping for GDELT articles in ES to allow filtering and aggregation by date

--- python/bigforecast/gdelt/split_gdelt.py
+++ python/bigforecast/gdelt/split_gdelt.py
@@ -12,7 +12,8 @@ def extract_fields(row):
     cols_to_keep = ["GlobalEventID", "Day", "MonthYear",
                     "DATEADDED", "SOURCEURL", "NumMentions",
                     "NumSources", "NumArticles", "AvgTone",
-                    "GoldsteinScale", "EventRootCode", "QuadClass"]
+                    "GoldsteinScale", "EventRootCode", "QuadClass",
+                    "timestamp"]
     return dict(row[cols_to_keep])
 
 
@@ -40,5 +41,31 @@ def split_v2_GDELT(update_file):
                           compression="zip",
                           names=cols)
 
+    # Convert Dates to proper date strings
+    gdeltDF['timestamp'] = gdeltDF['DATEADDED'].apply(_format_gdelt_date)
+
     # Return a list of rows (one per article)
     return list(gdeltDF.apply(extract_fields, 1))
+
+
+def _format_gdelt_date(date_num):
+    """
+    Given dates from GDELT of the form "20170819204500", convert to
+    proper timestamps of the form "2017-08-19 20:45:00".
+    """
+
+    # Check inputs
+    assert isinstance(date_num, int) or isinstance(date_num, float)
+
+    # Convert to character
+    date_str = str(int(date_num))
+
+    # Extract components. God I hope this doesn't break
+    out_str = date_str[:4] + "-" + \
+              date_str[4:6] + "-" + \
+              date_str[6:8] + " " + \
+              date_str[8:10] + ":" + \
+              date_str[10:12] + ":" + \
+              date_str[12:14]
+
+    return(out_str)
